b380b5fc37881afbe6be83be9274dfea4832925e
Auroriana <gabrielvincentreyes13@gmail.com>
2026-03-17 19:30:13 +0800

    JavaScript test

---
+++ javascript.js
@@ -0,0 +1,21 @@
+console.log(23 + 97);
+console.log(5 + 5 + 23 + 23 + 12312 + 234);
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+
+a = 32;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
